4e79072c9e8d5cb386e15b88ef3ff77a5147a451
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-03-01 16:28:43 +0000

    feat(ui): add main fragment and main activity toolbar
    Add main fragment and main activity toolbar with dummy buttons.

--- app/src/main/java/com/example/mentalmathsapp/MainActivity.java
+++ app/src/main/java/com/example/mentalmathsapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // disable night mode
+        // todo: implement night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
